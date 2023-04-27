1efab3e738930d8a9e60102ffdc3309688128cbf
ZacRayTho <zachary.thomas@student.awesomeinc.org>
2023-04-27 23:24:03 +0000

    fixing deployment errors

--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -1,15 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" />
+        <title>Marketopia</title>
+        <link rel="icon" href="./img/mtlogo.png" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Righteous&display=swap"
+        />
       </Head>
-      <body className='overflow-hidden'>
+      <body className="overflow-hidden">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
